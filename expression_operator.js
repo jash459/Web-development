3f47722be053b4a500b2e9b2b2a8ff396bcbff53
Jash Hinger <93431632+jash459@users.noreply.github.com>
2022-10-26 19:31:05 +0530

    Add files via upload

---
+++ expression_operator.js
@@ -0,0 +1,59 @@
+ console.log("operators in js")
+
+let a=23;
+let b=45;
+console.log("a + b :", a+b)
+console.log("a - b :", a-b)
+console.log("a % b :", a%b)
+console.log("a / b :", a/b)
+
+let comp1=6;
+let comp2="6"
+
+console.log("comp1 == comp2 is ", comp1 ==comp2) // true
+console.log("comp1 != comp2 is ", comp1 !=comp2)  // false
+console.log("comp1 === comp2 is ", comp1 ===comp2) // false
+console.log("comp1 !== comp2 is ", comp1 !==comp2)  // true
+
+console.log("you can", a<18 ? "not drive" : "drive")
+
+// (a == b) => false 
+// (a < b) => true 
+// (a > b) => false 
+// (a != b) => true 
+// (a >= b) => false 
+// a <= b) => true
+
+// (a && b) => false 
+// (a || b) => true 
+// !(a && b) => true
+
+// (a & b) => 2 
+// (a | b) => 3 
+// (a ^ b) => 1 
+// (~b) => -4 
+// (a << b) => 16 
+// (a >> b) => 0
+
+// Value of a => (a = b) => 10
+// Value of a => (a += b) => 20 
+// Value of a => (a -= b) => 10 
+// Value of a => (a *= b) => 100 
+// Value of a => (a /= b) => 10
+// Value of a => (a %= b) => 0
+
+// ((a > b) ? 100 : 200) => 200 
+// ((a < b) ? 100 : 200) => 100
+
+// result = (typeof b == "string" ? "B is String" : "B is Numeric");
+
+// result = (typeof a == "string" ? "A is String" : "A is Numeric");
+
+// a + b = 43
+// a - b = 23
+// a / b = 3.3
+// a % b = 3
+// a + b + c = 43Test
+// ++a = 35
+// --b = 8
+

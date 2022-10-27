e82497c6939697029c1aa59a0add5bc8b5007d46
Jash Hinger <93431632+jash459@users.noreply.github.com>
2022-10-27 18:57:06 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,79 @@
+let name ="Jash";
+console.log(name.length);
+
+console.log(name[0]);
+console.log(name[1]);
+let friend ='Prakesh';
+console.log(friend);
+
+let myfreind='hinger'
+console.log(myfreind);
+
+// Template literals
+let boy1='Harshit';
+let boy2='Naman';
+
+
+// Jash is a freind of motii
+let sentence='${boy1} is a friend of ${boy2}';
+console.log(sentence);
+
+// Escape sequences characters : \" , \n , \t 
+let fruit="Bana\na"
+console.log(fruit);
+
+
+// String Methods:
+// indexing start from 0;
+
+// String are immutable
+
+let na="Jash"
+console.log(na.length);
+
+console.log(toUpperCase());
+
+console.log(toLowerCase());
+
+na.slice(2,4)  // 2 to 4 but 4 not include
+
+na.slice(2) // from 2 end
+
+let name2 = "Hinger";
+let newname=name2.replace("Hinger","JASH")
+
+let name3="hinger"
+let name4="you"
+let name5=name3.concat(name4,"Yes") // we can use + operator
+
+let name6="   JASH    " // remove whitespaces
+let newname2=name6.trim();
+
+let friend2="Motii";
+console.log(namm6.concat("is a friend of " , friend , "Ok"))
+
+// String are immutable in order to access the character we use the following syntax:
+
+let friend4="motiiii"
+console.log(friend4[1]);  // o
+console.log(friend4[2]);  // o 
+
+let freind5 =  "JASH" + "MOTII" + "HINGER"
+console.log(freind5[0])
+console.log(freind5[1])
+console.log(freind5[2])
+freind5[3]="IIIIIIII"   // this is not possible
+
+console.log(freind5);
+
+
+ 
+
+
+
+  a
+ 
+
+
+
+
